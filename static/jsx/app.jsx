092239dec3a0e71a28e4d65d49867773415e7d3b
aaaasmile <testhaus@gmx.at>
2018-11-24 22:19:20 +0100

    update layout

--- static/jsx/app.jsx
+++ static/jsx/app.jsx
@@ -1,7 +1,19 @@
 class App extends React.Component {
   render() {
     return (
-      <p>Wow this is a APP!!</p>
+      <div>
+        <div className="ui minimal comments">
+          <h3 className="ui dividing header">Esplora</h3>
+          <div className="comment">
+          </div>
+        </div>
+        <div className="ui left icon action input">
+          <i className="search icon"></i>
+          <input type="text" placeholder="Cerca..."></input>
+          <button className="ui icon right attached primary button"><i className="paper plane icon"></i>
+          </button>
+        </div >
+      </div>
     )
   }
 }
