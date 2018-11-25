763e62c85fa4510478e342cc610c69d779d34138
aaaasmile <testhaus@gmx.at>
2018-11-25 22:24:21 +0100

    layout with post

--- static/jsx/app.jsx
+++ static/jsx/app.jsx
@@ -6,7 +6,6 @@ class App extends React.Component {
           <h3 className="ui dividing header">Esplora</h3>
         </div>
         <Commander></Commander>
-        <Posts></Posts>
       </div>
     )
   }
@@ -19,7 +18,7 @@ class Commander extends React.Component {
       posts: []
     };
 
-    this.serverRequest = this.serverRequest.bind(this);
+    //this.serverRequest = this.serverRequest.bind(this);
   }
 
   serverRequest(cmd) {
@@ -32,35 +31,62 @@ class Commander extends React.Component {
     console.log('POST to ', url)
     $.post(url, res => {
       console.log('Res is:', res)
+      var pp = JSON.parse(res)
+      this.setState({ posts: pp.Posts })
+      // pp.Posts.forEach(element => {
+      //console.log(element)
+      // });
     })
   }
 
   render() {
     return (
-      <div className="ui left icon action input">
-        <i className="search icon"></i>
-        <input id="contcmd" type="text" placeholder="Cerca..." onKeyUp={(ev) => {
-          if (ev.key === 'Enter') {
-            let val = $('#contcmd').val()
-            console.log('Enter recognized: ', val)
-            this.serverRequest(val)
-            $('#contcmd').val('')
-          }
-        }}></input>
-        <button className="ui icon right attached primary button"
-          onClick={() => {
-            this.serverRequest($('#contcmd').val())
-          }}><i className="paper plane icon"></i>
-        </button>
-      </div >
+      <div>
+        <div className="ui left icon action input">
+          <i className="search icon"></i>
+          <input id="contcmd" type="text" placeholder="Cerca..." onKeyUp={(ev) => {
+            if (ev.key === 'Enter') {
+              let val = $('#contcmd').val()
+              console.log('Enter recognized: ', val)
+              this.serverRequest(val)
+              $('#contcmd').val('')
+            }
+          }}></input>
+          <button className="ui icon right attached primary button"
+            onClick={() => {
+              this.serverRequest($('#contcmd').val())
+            }}><i className="paper plane icon"></i>
+          </button>
+        </div>
+        <div className="ui container">
+          <div className="comment">
+            {this.state.posts.map(function (post, i) {
+              return <Post key={i} post={post} />;
+            })}
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-class Posts extends React.Component {
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+
+  }
   render() {
     return (
-      <div className="comment">
+      <div className="content">
+        <a className="author">{this.props.post.UserName}</a>
+        <div className="metadata">
+          <span className="date">{this.props.post.Date}</span>
+        </div>
+        <div className="text">
+          {this.props.post.Content}
+        </div>
       </div>
     )
   }
