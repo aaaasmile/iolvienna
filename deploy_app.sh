6b37f460fd0ccd691bf1ba3d9eb6691183b56d3c
aaaasmile <testhaus@gmx.at>
2018-11-25 19:35:00 +0100

    recompiled app

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+go build --tags "fts5"
 sudo systemctl stop iolvienna
 rsync -av ./static/ /home/igor/app/go/iol-service/static
 rsync -av ./templates/ /home/igor/app/go/iol-service/templates
 #cp /home/igor/app/go/iol-service/config.toml /home/igor/app/go/iol-service/config.toml_old
-cp ./config.toml /home/igor/app/go/iol-service/config.toml
+#cp ./config.toml /home/igor/app/go/iol-service/config.toml
 cp ./iolvienna /home/igor/app/go/iol-service/iolvienna.bin
 sudo systemctl start iolvienna
--- static/dist/app.js
+++ static/dist/app.js
@@ -1,6 +1,73 @@
 class App extends React.Component {
   render() {
-    return React.createElement("p", null, "Wow this is a APP!!");
+    return React.createElement("div", null, React.createElement("div", {
+      className: "ui minimal comments"
+    }, React.createElement("h3", {
+      className: "ui dividing header"
+    }, "Esplora")), React.createElement(Commander, null), React.createElement(Posts, null));
+  }
+
+}
+
+class Commander extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+    this.serverRequest = this.serverRequest.bind(this);
+  }
+
+  serverRequest(cmd) {
+    if (!cmd) {
+      console.log('cmd is empty');
+      return;
+    }
+
+    var ser = $.param({
+      "req": cmd
+    });
+    var url = 'do?' + ser;
+    console.log('POST to ', url);
+    $.post(url, res => {
+      console.log('Res is:', res);
+    });
+  }
+
+  render() {
+    return React.createElement("div", {
+      className: "ui left icon action input"
+    }, React.createElement("i", {
+      className: "search icon"
+    }), React.createElement("input", {
+      id: "contcmd",
+      type: "text",
+      placeholder: "Cerca...",
+      onKeyUp: ev => {
+        if (ev.key === 'Enter') {
+          let val = $('#contcmd').val();
+          console.log('Enter recognized: ', val);
+          this.serverRequest(val);
+          $('#contcmd').val('');
+        }
+      }
+    }), React.createElement("button", {
+      className: "ui icon right attached primary button",
+      onClick: () => {
+        this.serverRequest($('#contcmd').val());
+      }
+    }, React.createElement("i", {
+      className: "paper plane icon"
+    })));
+  }
+
+}
+
+class Posts extends React.Component {
+  render() {
+    return React.createElement("div", {
+      className: "comment"
+    });
   }
 
 }
