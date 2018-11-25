b4a61be0777ebdcd96622da50b3aadf7149cc0a7
aaaasmile <testhaus@gmx.at>
2018-11-25 19:43:02 +0100

    update deploy

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-go build --tags "fts5"
-sudo systemctl stop iolvienna
-rsync -av ./static/ /home/igor/app/go/iol-service/static
-rsync -av ./templates/ /home/igor/app/go/iol-service/templates
-#cp /home/igor/app/go/iol-service/config.toml /home/igor/app/go/iol-service/config.toml_old
-#cp ./config.toml /home/igor/app/go/iol-service/config.toml
-cp ./iolvienna /home/igor/app/go/iol-service/iolvienna.bin
-sudo systemctl start iolvienna
+read -p "Deploy the iolservice (vienna.invido.it)? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+	echo "build application"
+	go build --tags "fts5"
+	echo "stop the service"
+	sudo systemctl stop iolvienna
+	echo "sync static files"
+	rsync -av ./static/ /home/igor/app/go/iol-service/static
+	rsync -av ./templates/ /home/igor/app/go/iol-service/templates
+	#cp /home/igor/app/go/iol-service/config.toml /home/igor/app/go/iol-service/config.toml_old
+	#cp ./config.toml /home/igor/app/go/iol-service/config.toml
+	echo "Copy the  service binary"
+	cp ./iolvienna /home/igor/app/go/iol-service/iolvienna.bin
+	sudo systemctl start iolvienna
+	echo "Service restarted"
+fi
+echo "That's all folks!"
