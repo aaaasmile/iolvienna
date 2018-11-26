ed839c7d3a7628f50d2d4e9fb06ab2dcb6947746
aaaasmile <testhaus@gmx.at>
2018-11-26 01:01:26 +0100

    update app.js

--- static/dist/app.js
+++ static/dist/app.js
@@ -4,7 +4,7 @@ class App extends React.Component {
       className: "ui minimal comments"
     }, React.createElement("h3", {
       className: "ui dividing header"
-    }, "Esplora")), React.createElement(Commander, null), React.createElement(Posts, null));
+    }, "Esplora")), React.createElement(Commander, null));
   }
 
 }
@@ -15,7 +15,7 @@ class Commander extends React.Component {
     this.state = {
       posts: []
     };
-    this.serverRequest = this.serverRequest.bind(this);
+    this.callMorePostsOnDate = this.callMorePostsOnDate.bind(this);
   }
 
   serverRequest(cmd) {
@@ -31,11 +31,67 @@ class Commander extends React.Component {
     console.log('POST to ', url);
     $.post(url, res => {
       console.log('Res is:', res);
+      var pp = JSON.parse(res);
+      this.setState({
+        posts: pp.Posts
+      }); // pp.Posts.forEach(element => {
+      //console.log(element)
+      // });
     });
   }
 
+  callMorePostsOnDate(date) {
+    var ser = $.param({
+      "date": date
+    });
+    var url = 'do?' + ser;
+    console.log('POST to ', url);
+    $.post(url, res => {
+      console.log('Res is:', res);
+      var pp = JSON.parse(res);
+      this.setState({
+        posts: pp.Posts
+      });
+    });
+  }
+
+  movePostsOnDate(forw) {
+    if (this.state.posts.length > 0) {
+      var date;
+      var ser;
+
+      if (forw) {
+        // forward
+        let last = this.state.posts[this.state.posts.length - 1];
+        date = last.Date;
+        ser = $.param({
+          "datemore": date
+        });
+      } else {
+        // backward
+        let first = this.state.posts[0];
+        date = first.Date;
+        ser = $.param({
+          "dateless": date
+        });
+      }
+
+      var url = 'do?' + ser;
+      console.log('POST to ', url);
+      $.post(url, res => {
+        //console.log('Res is:', res)
+        var pp = JSON.parse(res);
+        this.setState({
+          posts: pp.Posts
+        });
+      });
+    }
+  }
+
   render() {
-    return React.createElement("div", {
+    var that = this; // importante per chiamare callMorePostsOnDate
+
+    return React.createElement("div", null, React.createElement("div", {
       className: "ui left icon action input"
     }, React.createElement("i", {
       className: "search icon"
@@ -58,16 +114,52 @@ class Commander extends React.Component {
       }
     }, React.createElement("i", {
       className: "paper plane icon"
-    })));
+    }))), React.createElement("div", {
+      className: "ui"
+    }, React.createElement("div", {
+      className: "comment"
+    }, this.state.posts.map(function (post, i) {
+      return React.createElement(Post, {
+        key: i,
+        post: post,
+        morePostsOnDate: that.callMorePostsOnDate
+      });
+    })), this.state.posts && this.state.posts.length > 0 ? React.createElement("div", null, React.createElement("a", {
+      onClick: () => this.movePostsOnDate(false)
+    }, React.createElement("i", {
+      className: "backward icon"
+    })), " ", React.createElement("a", {
+      onClick: () => this.movePostsOnDate(true)
+    }, React.createElement("i", {
+      className: "forward icon"
+    }))) : null));
   }
 
 }
 
-class Posts extends React.Component {
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return React.createElement("div", {
-      className: "comment"
-    });
+      className: "ui postId"
+    }, React.createElement("div", {
+      className: "content"
+    }, React.createElement("div", {
+      className: "author"
+    }, this.props.post.UserName), React.createElement("div", {
+      className: "metadata"
+    }, React.createElement("a", {
+      className: "date",
+      onClick: () => {
+        this.props.morePostsOnDate(this.props.post.Date);
+      }
+    }, this.props.post.Date)), React.createElement("div", {
+      className: "text"
+    }, this.props.post.Content)));
   }
 
 }
