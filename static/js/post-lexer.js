35b7e21ddcbdf138a07b4d9c7961b201708e84d0
aaaasmile <testhaus@gmx.at>
2018-12-12 23:08:24 +0100

    lexer recognize cr

--- static/js/post-lexer.js
+++ static/js/post-lexer.js
@@ -3,24 +3,31 @@ const passPhrase = "ciao a tutti, come va?!\rMa mi sbaglio ho si voleva fare un'
 
 const lex = {};
 (function () {
-  const _itemText = 0;
-  const _itemLfCr = 1;
+  const tokType_Text = 0;
+  const tokType_LfCr = 1;
 
   lex.procPost = function (post) {
     let l = lex.lexCtor("Post lex", post)
     let rr = ""
+    let exit = false
     while (1) {
       let item = l.nextItem()
       if (!item) {
         break
       }
       console.log("type %s, val %s", item.typ, item.val)
-      if (item.typ == _itemLfCr) {
-        rr += "<br />"
-      } else if (item.typ === _itemText) {
-        rr += item.val
+      switch (item.typ) {
+        case tokType_LfCr:
+          rr += "<br />"
+          break
+        case tokType_Text:
+          rr += item.val
+          break
+        default:
+          exit = true
+          break
       }
-      if (l.state === null) {
+      if ((l.state === null) || exit) {
         break
       }
     }
@@ -32,41 +39,64 @@ const lex = {};
       name: name,
       input: input,
       state: null,
-      items: [],
       start: 0,
       pos: 0,
+      prev_pos: -1,
+      prev_state: null,
     };
 
     _lexer.nextItem = function () {
-      if (_lexer.items.length === 0) {
-        let _item = {
-          typ: _itemText,
-          val: _lexer.input
-        }
-        _lexer.state = null
-        return _item;
-      } else {
-        return _lexer.items.pop();
+      if (!_lexer.state) {
+        return null
       }
+
+      if ((_lexer.prev_state === _lexer.state) &&
+        (_lexer.prev_pos === _lexer.pos)) {
+        throw ("State stale")
+      }
+      _lexer.prev_pos = _lexer.pos
+      _lexer.prev_state = _lexer.state
+
+      let item = null
+      _lexer.state = _lexer.state(x => {
+        item = {
+          typ: x,
+          val: _lexer.input.substr(_lexer.start, _lexer.pos - _lexer.start)
+        }
+      })
+      return item
     }
 
-    _lexer.emit = function (item) {
-      _lexer.items.push(item)
+    _lexer.nextFn = function () {
+      _lexer.pos += 1
+      return _lexer.pos >= _lexer.input.length ? null : _lexer.pos
     }
 
-    _lexer.lexText = function () {
+    _lexer.stCrLf = function (fnCbTyp) {
+      _lexer.pos += 1
+      _lexer.start = _lexer.pos
+      fnCbTyp(tokType_LfCr)
+      return _lexer.stText
+    }
+
+    _lexer.stText = function (fnCbTyp) {
       while (1) {
-        if (_lexer.next() === null) {
+        // todo check for rule and 
+        if (_lexer.input[_lexer.pos] === '\r') {
+          fnCbTyp(tokType_Text)
+          return _lexer.stCrLf // next state
+        }
+        if (_lexer.nextFn() === null) {
           break
         }
       }
       if (_lexer.pos > _lexer.start) {
-        _lexer.emit(itemText)
+        fnCbTyp(tokType_Text)
       }
       return null
     }
 
-    _lexer.state = _lexer.lexText
+    _lexer.state = _lexer.stText // initial state
 
     return _lexer
   }
