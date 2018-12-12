c5c34caa78f46f5b6465e7586d72ca215e81f6f4
aaaasmile <testhaus@gmx.at>
2018-12-13 00:49:28 +0100

    works crlf -ok

--- static/js/post-lexer.js
+++ static/js/post-lexer.js
@@ -1,10 +1,41 @@
 //const passPhrase = "ciao a tutti, come va?!\rMa mi sbaglio ho si voleva fare un' apero?   \r\r Surf World Cup dal 1.-4.05 Podersdorf \rChi fosse interessato a biglietti gratuiti mi faccia sapere.\r\ra presto"
-const passPhrase = "Josè lumas De Oliveira, mai sentito(sul fmaigerato google ho trovato questo oltre l'imagine di un vecchietto:[\"http://www.jornalprimeirahora.com.br/imagens/noticias/luma_21_05_08.jpg\":http://www.jornalprimeirahora.com.br/imagens/noticias/luma_21_05_08.jpg])\rOra però hai stuzzicato la curiosità cho è costui? \r\rJavier Bardem è lui ---\u003e [\"http://www.contactmusic.com/pics/m/oscars_pressroom_240208/javier_bardem_5095236.jpg\":http://www.contactmusic.com/pics/m/oscars_pressroom_240208/javier_bardem_5095236.jpg] ultimo premio oscar come miglior attore maschile \r\rAhhh questo terrence qua: [\"http://lh5.ggpht.com/_YTsfWmtb6gY/SJMD9qzvYKI/AAAAAAAAAKk/YjN5o3Amsps/candy+e+terence4.jpg\":http://lh5.ggpht.com/_YTsfWmtb6gY/SJMD9qzvYKI/AAAAAAAAAKk/YjN5o3Amsps/candy+e+terence4.jpg] sorry nna vevo letto bene... \r\rma ribatto ke a me candy candy annoiava  era + poteten si qualsiasi altro sonnifero!\r\rL'opera magari un'altra volta!"
+//const passPhrase = "Josè lumas De Oliveira, mai sentito(sul fmaigerato google ho trovato questo oltre l'imagine di un vecchietto:[\"http://www.jornalprimeirahora.com.br/imagens/noticias/luma_21_05_08.jpg\":http://www.jornalprimeirahora.com.br/imagens/noticias/luma_21_05_08.jpg])\rOra però hai stuzzicato la curiosità cho è costui? \r\rJavier Bardem è lui ---\u003e [\"http://www.contactmusic.com/pics/m/oscars_pressroom_240208/javier_bardem_5095236.jpg\":http://www.contactmusic.com/pics/m/oscars_pressroom_240208/javier_bardem_5095236.jpg] ultimo premio oscar come miglior attore maschile \r\rAhhh questo terrence qua: [\"http://lh5.ggpht.com/_YTsfWmtb6gY/SJMD9qzvYKI/AAAAAAAAAKk/YjN5o3Amsps/candy+e+terence4.jpg\":http://lh5.ggpht.com/_YTsfWmtb6gY/SJMD9qzvYKI/AAAAAAAAAKk/YjN5o3Amsps/candy+e+terence4.jpg] sorry nna vevo letto bene... \r\rma ribatto ke a me candy candy annoiava  era + poteten si qualsiasi altro sonnifero!\r\rL'opera magari un'altra volta!"
+const passPhrase = "a\rb"
 
 const lex = {};
 (function () {
-  const tokType_Text = 0;
-  const tokType_LfCr = 1;
+  const tokType_Text = 0
+  const tokType_LfCr = 1
+  const tokType_UrlLabel = 2
+  const tokType_UrlLink = 3
+
+  lex.hexEncode = function (str) {
+    let hex, i;
+
+    let result = "";
+    for (i = 0; i < str.length; i++) {
+      hex = str.charCodeAt(i).toString(16);
+      if (result !== '') {
+        result += ' '
+      }
+      result += ("000" + hex).slice(-4);
+    }
+    return result
+  }
+
+  lex.debugType = function (typ) {
+    switch (typ) {
+      case tokType_Text:
+        return "Text"
+      case tokType_LfCr:
+        return "LfCr"
+      case tokType_UrlLabel:
+        return "UrlLabel"
+      case tokType_UrlLink:
+        return "UrlLink"
+    }
+    return "typ is unknown"
+  }
 
   lex.procPost = function (post) {
     let l = lex.lexCtor("Post lex", post)
@@ -15,7 +46,7 @@ const lex = {};
       if (!item) {
         break
       }
-      console.log("type %s, val %s", item.typ, item.val)
+      console.log("%d %s val: %s (%s)", item.typ, lex.debugType(item.typ), item.val, lex.hexEncode(item.val))
       switch (item.typ) {
         case tokType_LfCr:
           rr += "<br />"
@@ -69,22 +100,71 @@ const lex = {};
 
     _lexer.next = function () {
       _lexer.pos += 1
-      return _lexer.pos >= _lexer.input.length ? null : _lexer.pos
+      return _lexer.pos >= _lexer.input.length ? null : _lexer.input[_lexer.pos]
+    }
+
+    _lexer.ignore = function () {
+      _lexer.start = _lexer.pos
+    }
+
+    _lexer.backup = function () {
+      if (_lexer.pos > 0) {
+        _lexer.pos -= 1
+      }
+    }
+
+    _lexer.peek = function () {
+      let pn = _lexer.next()
+      _lexer.backup()
+      return pn
+    }
+
+    _lexer.stBraLeftLink = function (fnCbTyp) {
+      _lexer.pos += 1
+      _lexer.start = _lexer.pos
+      while (1) {
+        if (_lexer.peek() === ']') {
+          fnCbTyp(tokType_UrlLink)
+          return _lexer.stText
+        }
+        if (_lexer.next() === null) {
+          break
+        }
+      }
+      return _lexer.stText
+    }
+
+    _lexer.stBraLeftLabel = function (fnCbTyp) {
+      _lexer.pos += 1
+      _lexer.start = _lexer.pos
+      while (1) {
+        if (_lexer.input[_lexer.pos] === ':') {
+          fnCbTyp(tokType_UrlLabel)
+          return _lexer.stBraLeftLink
+        }
+        if (_lexer.next() === null) {
+          return null
+        }
+      }
+      return _lexer.stText
     }
 
     _lexer.stCrLf = function (fnCbTyp) {
-      _lexer.pos += 1
-      _lexer.start = _lexer.pos
+      let r = _lexer.next()
+      _lexer.ignore()
       fnCbTyp(tokType_LfCr)
-      return _lexer.stText
+      return r ? _lexer.stText : null
     }
 
     _lexer.stText = function (fnCbTyp) {
       while (1) {
-        // todo check for rule and 
         if (_lexer.input[_lexer.pos] === '\r') {
           fnCbTyp(tokType_Text)
           return _lexer.stCrLf // next state
+        }
+        else if (_lexer.input[_lexer.pos] === '[') {
+          fnCbTyp(tokType_Text)
+          return _lexer.stBraLeft // next state
         }
         if (_lexer.next() === null) {
           break
@@ -103,4 +183,6 @@ const lex = {};
 })();
 
 //console.log(_itemText)
-console.log('Process post: ', lex.procPost(passPhrase))
+console.log('Input is:', passPhrase)
+const strHtml = lex.procPost(passPhrase)
+console.log('Process post: %s, len %d', strHtml, strHtml.length)
